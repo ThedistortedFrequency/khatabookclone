dfdce296f7c9da486f96a2cde45e50425ad42357
Pritam Sharma <pritamsharma3648@gmai.com>
2024-09-18 15:27:29 +0530

    added a pdf code

--- lib/bottamnavigator/parties.dart
+++ lib/bottamnavigator/parties.dart
@@ -1,11 +1,13 @@
-
 import 'package:khatabookclone/addcustomer.dart';
 import 'package:khatabookclone/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:khatabookclone/widgets/customfab.dart';
 import 'package:lucide_icons/lucide_icons.dart';
+import 'package:pdf/pdf.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 
 class UserList extends StatefulWidget {
@@ -311,63 +313,277 @@ class _UserListState extends State<UserList> {
 class PersonDetailScreen extends StatelessWidget {
   final String userId;
 
-  const PersonDetailScreen({Key? key, required this.userId}) : super(key: key);
+  PersonDetailScreen({Key? key, required this.userId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance.collection('users').doc(userId).get(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(child: Text('Error: ${snapshot.error}')),
-          );
-        }
-
-        if (!snapshot.hasData || !snapshot.data!.exists) {
-          return const Scaffold(
-            body: Center(child: Text('No data available')),
-          );
-        }
-
-        var user = snapshot.data!.data() as Map<String, dynamic>;
-        var creditBalance = user['Transaction Type'] == 'Credit'
-            ? user['Amount']
-            : 0;
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('${user['Name']}  '),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Details'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EditUserScreen(userId: userId),
+                ),
+              );
+            },
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Name: ${user['Name']}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 16.0),
-                Text('Amount: ₹${user['Amount'].toStringAsFixed(0)}'),
-                Text('Interest Rate: ${user['Interest'].toStringAsFixed(2)}%'),
-                Text('Transaction Type: ${user['Transaction Type']}'),
-                // Add more details as needed
-              ],
+          IconButton(
+            icon: const Icon(Icons.download),
+            onPressed: () {
+              _generatePdf(context as double, userId);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // User details section
+          FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance.collection('users')
+                .doc(userId)
+                .get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+
+              if (!snapshot.hasData || !snapshot.data!.exists) {
+                return const Center(child: Text('No data available'));
+              }
+
+              var user = snapshot.data!.data() as Map<String, dynamic>;
+
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name: ${user['Name']}', style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 16.0),
+                    Text('Total Amount: ₹${user['Amount'] ?? 0}'),
+                    Text('Interest Rate: ${user['Interest'] ?? 0}%'),
+                    Text('Transaction Type: ${user['Transaction Type']}'),
+                  ],
+                ),
+              );
+            },
+          ),
+          // Transactions list section
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(userId)
+                  .collection(
+                  'transactions') // Assuming transactions are stored under user document
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                var transactions = snapshot.data?.docs ?? [];
+
+                if (transactions.isEmpty) {
+                  return const Center(child: Text('No transactions available'));
+                }
+
+                return ListView(
+                  children: transactions.map((doc) {
+                    var transaction = doc.data() as Map<String, dynamic>;
+                    var amount = transaction['Amount'] ?? 0.0;
+                    var transactionType = transaction['Transaction Type'] ??
+                        'Unknown';
+                    var timestamp = transaction['timestamp']?.toDate() ??
+                        DateTime.now();
+
+                    Color backgroundColor = transactionType == 'Credit'
+                        ? Colors.green[50]!
+                        : Colors.red[50]!;
+                    Color numberColor = transactionType == 'Credit' ? Colors
+                        .green : Colors.red;
+
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4.0, horizontal: 8.0),
+                      child: Container(
+                        color: backgroundColor,
+                        child: ListTile(
+                          title: Text(transactionType),
+                          subtitle: Text(timeago.format(timestamp)),
+                          trailing: Text(
+                            '₹${amount.toStringAsFixed(0)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: numberColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddTransactionDialog(context, userId);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+
+  void _showAddTransactionDialog(BuildContext context, String userId) {
+    String transactionType = 'Credit';
+    final amountController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Transaction'),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: amountController,
+                    decoration: const InputDecoration(labelText: 'Amount'),
+                    keyboardType: TextInputType.number,
+                  ),
+                  Row(
+                    children: [
+                      Radio<String>(
+                        value: 'Credit',
+                        groupValue: transactionType,
+                        onChanged: (value) {
+                          setState(() {
+                            transactionType = value!;
+                          });
+                        },
+                      ),
+                      const Text('Credit'),
+                      Radio<String>(
+                        value: 'Debit',
+                        groupValue: transactionType,
+                        onChanged: (value) {
+                          setState(() {
+                            transactionType = value!;
+                          });
+                        },
+                      ),
+                      const Text('Debit'),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
+          actions: [
+            TextButton(
+
+              onPressed: () {
+                double amount = double.tryParse(amountController.text) ?? 0.0;
+                if (amount > 0) {
+                  // Add transaction to Firestore
+                  FirebaseFirestore.instance.collection('users')
+                      .doc(userId)
+                      .collection('transactions')
+                      .add({
+                    'Amount': amount,
+                    'Transaction Type': transactionType,
+                    'timestamp': FieldValue.serverTimestamp(),
+                  });
+
+                  // Generate PDF
+                  _generatePdf(amount, transactionType);
+
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Add'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
         );
       },
     );
+  }
+
+  Future<void> _generatePdf(double amount, String transactionType) async {
+    try {
+      final pdf = pw.Document();
+
+      pdf.addPage(
+        pw.Page(
+          build: (pw.Context context) {
+            return pw.Center(
+              child: pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                children: [
+                  pw.Text(
+                      'Transaction Receipt', style: pw.TextStyle(fontSize: 24)),
+                  pw.SizedBox(height: 20),
+                  pw.Text('Amount: ₹${amount.toStringAsFixed(2)}',
+                      style: pw.TextStyle(fontSize: 18)),
+                  pw.Text('Transaction Type: $transactionType',
+                      style: pw.TextStyle(fontSize: 18)),
+                  pw.SizedBox(height: 20),
+                  pw.Text('Thank you for your transaction!',
+                      style: pw.TextStyle(fontSize: 16)),
+                ],
+              ),
+            );
+          },
+        ),
+      );
+
+      // Generate PDF and handle errors
+      await Printing.layoutPdf(
+        onLayout: (PdfPageFormat format) async {
+          print("Generating PDF...");
+          return pdf.save();
+        },
+      );
+
+      print("PDF generated successfully!");
+    } catch (e) {
+      print("Error generating PDF: $e");
+      // Show a Snackbar or dialog with the error message
+      // e.g., ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: $e')));
+    }
   }
 }
 
 
 
-class EditUserScreen extends StatefulWidget {
+
+  class EditUserScreen extends StatefulWidget {
   final String userId;
   const EditUserScreen({super.key, required this.userId});
 
